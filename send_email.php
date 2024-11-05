06ee8308e47903c04d3338d4fa84f150b311e9e5
Onur <lexa1337onur@proton.me>
2024-11-06 01:14:14 +0300

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formdan gelen verileri al
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // E-posta bilgileri
+    $to = "onursametcelik2023@gmail.com"; // E-posta alıcısı
+    $subject = "Yeni Mesaj: $name";
+    $body = "Ad: $name\nE-posta: $email\nMesaj: $message";
+    $headers = "From: $email";
+
+    // E-posta gönderme
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mesajınız başarıyla gönderildi!";
+    } else {
+        echo "Bir hata oluştu, lütfen tekrar deneyin.";
+    }
+}
+?>
